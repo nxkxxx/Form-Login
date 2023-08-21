72922385309a01081237b4942217af6aee5675a4
nxkxxx <nonedity@gmail.com>
2023-08-21 13:32:12 +0800

    Modif komen sedikit

--- main.js
+++ main.js
@@ -5,17 +5,19 @@ const showPasswordCheckbox = document.getElementById('showPassword')
 form.addEventListener('submit', function (event) { // Menambahkan event listener pada saat form disubmit
     event.preventDefault(); // Mencegah form dikirim secara default
 
-    const email = document.getElementById('email').value; // Mendapatkan nilai email dan password dari input
+    // Mendapatkan nilai email dan password dari input
+    const email = document.getElementById('email').value; 
     const password = document.getElementById('password').value;
 
-    localStorage.setItem('email', email); // Menyimpan email dan password ke dalam local storage
+    // Menyimpan email dan password ke dalam local storage
+    localStorage.setItem('email', email); 
     localStorage.setItem('password', password);
 });
 
 showPasswordCheckbox.addEventListener('change', function () {
-    passwordInput.type = this.checked ? 'text' : 'password'; // Membuat tipe password menjadi 'text' ketika checkbox di-check, dan merubah inputnya menjadi 'password ketika di-uncheck
+    // Membuat tipe password menjadi 'text' ketika checkbox di-check, dan merubah inputnya menjadi 'password ketika di-uncheck
+    passwordInput.type = this.checked ? 'text' : 'password'; 
 })
-
 
 
 // CARA LAINNYA
